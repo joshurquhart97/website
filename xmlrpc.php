4c3958f81ed4229382a6cf4c3bc9a72baa772c8e
Josh Urquhart <joshurquhart97@live.com>
2023-11-15 19:16:42 +0000

    init

---
+++ xmlrpc.php
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>http://77.68.115.60/plesk-site-preview/www.sellpropertyedinburgh.com/https/77.68.115.60/</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="http://77.68.115.60/plesk-site-preview/www.sellpropertyedinburgh.com/https/77.68.115.60/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="http://77.68.115.60/plesk-site-preview/www.sellpropertyedinburgh.com/https/77.68.115.60/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://77.68.115.60/plesk-site-preview/www.sellpropertyedinburgh.com/https/77.68.115.60/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="http://77.68.115.60/plesk-site-preview/www.sellpropertyedinburgh.com/https/77.68.115.60/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="http://77.68.115.60/plesk-site-preview/www.sellpropertyedinburgh.com/https/77.68.115.60/wp-json/" />
+			</apis>
+	</service>
+</rsd>
